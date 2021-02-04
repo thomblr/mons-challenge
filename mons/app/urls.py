325cdbb2e392bb68afe2a0199a9b3bfbd8fdabe5
thomblr <thomas.1111@live.be>
2021-02-04 16:14:10 +0100

    New urls

--- mons/app/urls.py
+++ mons/app/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name="index"),
     path('profile/', views.profile, name="profile"),
     path('preferences/', views.preferences, name="preferences"),
     path('destination/', views.destination, name="destination"),
+    path('trajets/', views.trajets, name="trajets"),
 ]
